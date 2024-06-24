1446781b03095aedd0b9d080341dd73f8baca3b7
En Yang Du <377153400@qq.com>
2024-06-24 20:34:02 +0800

    feat(popup): add popup 🎉 (#59)
    * feat(popup): add popup 🎉
    
    * feat(popup): add popup 🎉

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './button';
 export * from './icon';
+export * from './input';
+export * from './popup';
 export * from './textarea';
 export * from './space';
 export * from './switch';
--- src/textarea/_example/base.tsx
+++ src/textarea/_example/base.tsx
@@ -1,12 +1,14 @@
 import 'tdesign-web-components/textarea';
 
 export default function Textarea() {
-
   return (
     <div style={{ gap: 16, display: 'flex', flexDirection: 'column' }}>
-        <t-textarea placeholder="请输入描述文案"></t-textarea>
-        <t-textarea placeholder="请输入文案，高度可自适应；autosize=true" autosize={true}></t-textarea>
-        <t-textarea placeholder="请输入文案，高度可自适应，最小3行，最大5行；autosize={minRows: 3, maxRows: 5}" autosize={{minRows: 3, maxRows: 5}}></t-textarea>
-        </div>
+      <t-textarea placeholder="请输入描述文案"></t-textarea>
+      <t-textarea placeholder="请输入文案，高度可自适应；autosize=true" autosize={true}></t-textarea>
+      <t-textarea
+        placeholder="请输入文案，高度可自适应，最小3行，最大5行；autosize={minRows: 3, maxRows: 5}"
+        autosize={{ minRows: 3, maxRows: 5 }}
+      ></t-textarea>
+    </div>
   );
 }
