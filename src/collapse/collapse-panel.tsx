b8ce30035a195e743747e0e0862d3bc44794c379
novlan1 <1576271227@qq.com>
2024-07-02 01:19:31 +0800

    feat(collapse): add transition for collapse

--- src/collapse/collapse-panel.tsx
+++ src/collapse/collapse-panel.tsx
@@ -1,22 +1,44 @@
 import '../common/fake-arrow';
+import 'omi-transition';
 
 import { bind, Component, computed, signal, tag } from 'omi';
 
 import classname, { classPrefix } from '../_util/classname';
 import { StyledProps, TNode } from '../common';
+import { getCollapseAnimation } from './collapse-animation';
 import type { TdCollapsePanelProps } from './type';
 
 export interface CollapsePanelProps extends TdCollapsePanelProps, StyledProps {}
 
+const { beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave } = getCollapseAnimation();
+
 @tag('t-collapse-panel')
 export default class CollapsePanel extends Component<TdCollapsePanelProps> {
   static isLightDom = true;
+
+  // static css = `
+  //   .t-slide-down-leave-to,
+  //   .t-slide-down-enter-from {
+  //     height: 0;
+  //   }
+
+  //   .t-slide-down-leave-from,
+  //   .t-slide-down-enter-to {
+  //     height: 100%;
+  //   }
+
+  //   .t-slide-down-leave-active,
+  //   .t-slide-down-enter-active {
+  //     // transition: all 500ms ease-in;
+  //   }`
 
   innerValue = signal(0);
 
   className = `${classPrefix}-collapse-panel`;
 
   isDisabled = signal(false);
+
+  afterLeaved = signal(false);
 
   install(): void {
     const { getUniqId, updateCollapseValue, defaultExpandAll } = this.injection;
@@ -76,6 +98,18 @@ export default class CollapsePanel extends Component<TdCollapsePanelProps> {
     event.stopPropagation();
   }
 
+  @bind
+  afterLeave(dom) {
+    afterLeave(dom);
+    this.afterLeaved.value = true;
+  }
+
+  @bind
+  beforeEnter(dom) {
+    beforeEnter(dom);
+    this.afterLeaved.value = false;
+  }
+
   renderIcon() {
     const { expandIconPlacement, collapseValue } = this.injection;
     const isActive = collapseValue.value.includes(this.innerValue.value);
@@ -120,15 +154,23 @@ export default class CollapsePanel extends Component<TdCollapsePanelProps> {
 
   renderBody() {
     const isActive = this.injection.collapseValue.value.includes(this.innerValue.value);
+    const { destroyOnCollapse } = this.props;
 
-    const { destroyOnCollapse, children } = this.props;
-
-    return destroyOnCollapse && !isActive ? null : (
-      <div style={!destroyOnCollapse && !isActive ? 'display: none;' : ''} className={`${this.className}__body`}>
-        <div className={`${this.className}__content`}>
-          {this.getDefaultSlot()}
-          {children}
-        </div>
+    return destroyOnCollapse && !isActive && this.afterLeaved.value ? null : (
+      <div
+        o-transition={{
+          name: `${classPrefix}-slide-down`,
+          beforeEnter: this.beforeEnter,
+          enter,
+          afterEnter,
+          beforeLeave,
+          leave,
+          afterLeave: this.afterLeave,
+        }}
+        className={`${this.className}__body`}
+        show={isActive}
+      >
+        <div className={`${this.className}__content`}>{this.getDefaultSlot()}</div>
       </div>
     );
   }
