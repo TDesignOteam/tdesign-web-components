fd77e977788861f4c35d97af5350099c6c9a0670
novlan1 <1576271227@qq.com>
2024-07-02 16:15:28 +0800

    chore(slider): update demo

--- src/slider/_example/min-and-max.tsx
+++ src/slider/_example/min-and-max.tsx
@@ -1,7 +1,7 @@
 import 'tdesign-web-components/slider';
 import 'tdesign-web-components/space';
 
-import { Component, signal } from 'omi';
+import { bind, Component, signal } from 'omi';
 
 export default class Demo extends Component {
   value1 = signal(12);
@@ -17,11 +17,29 @@ export default class Demo extends Component {
     30: 'max:30',
   });
 
+  @bind
+  onChange(value) {
+    console.log('[change]', value);
+  }
+
   render() {
     return (
       <t-space direction="vertical" size="60px" style={{ width: '100%' }}>
-        <t-slider value={this.value1.value} min={this.min.value} max={this.max.value} marks={this.marks.value} />
-        <t-slider range value={this.value2.value} min={this.min.value} max={this.max.value} marks={this.marks.value} />
+        <t-slider
+          value={this.value1.value}
+          min={this.min.value}
+          max={this.max.value}
+          marks={this.marks.value}
+          onChange={this.onChange}
+        />
+        <t-slider
+          range
+          value={this.value2.value}
+          min={this.min.value}
+          max={this.max.value}
+          marks={this.marks.value}
+          onChange={this.onChange}
+        />
       </t-space>
     );
   }
