9b98b936a5d676b929903c9173cc718994cec271
novlan1 <1576271227@qq.com>
2024-07-08 10:51:00 +0800

    chore: remove theme

--- src/style/index.js
+++ src/style/index.js
@@ -1,12 +1,10 @@
 import { css, globalCSS } from 'omi';
 
 import globalStye from  '../_common/style/web/_global.less';
-import themeStyle from '../_common/style/web/theme/_index.less';
 
 
 export const styleSheet = css`
   ${globalStye}
-  ${themeStyle}
 `;
 
 globalCSS(styleSheet);
