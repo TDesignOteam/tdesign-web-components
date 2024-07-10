7bb84e73712c836ffb2fecd8a5c4cc4c02c58aea
601odd <oddpro601@gmail.com>
2024-07-10 14:32:59 +0800

    chore(affix): 规范propsType类型 && 删去不必要的ref,signal

--- src/affix/affix.tsx
+++ src/affix/affix.tsx
@@ -1,5 +1,5 @@
 import { isFunction } from 'lodash';
-import { Component, createRef, signal, tag } from 'omi';
+import { Component, createRef, tag } from 'omi';
 
 import { getClassPrefix } from '../_util/classname';
 import { getScrollContainer } from '../_util/dom';
@@ -13,113 +13,112 @@ export default class Affix extends Component<AffixProps> {
   static defaultProps = { container: () => window, offsetBottom: 0, offsetTop: 0 };
 
   static propsType = {
-    children: Object,
-    container: Object,
-    content: Object,
+    children: Function || Object || String || Number,
+    container: Function || Object || String || Number,
+    content: Function || Object || String || Number,
     offsetBottom: Number,
     offsetTop: Number,
     zIndex: Number,
     onFixedChange: Function,
   };
 
-  innerOffsetTop = signal(0);
+  innerOffsetTop = 0;
 
-  inneroffsetBottom = signal(0);
+  inneroffsetBottom = 0;
 
-  innerZIndex = signal(0);
+  innerZIndex = 0;
 
   affixWrapRef = createRef<HTMLDivElement>();
 
   affixRef = createRef<HTMLDivElement>();
 
-  scrollContainer = createRef<ScrollContainerElement>();
+  scrollContainer: ScrollContainerElement = null;
 
-  placeholderEL = createRef<HTMLElement>();
+  placeholderEL: HTMLDivElement = null;
 
-  ticking = createRef<boolean>();
+  ticking = false;
 
   handleScroll = () => {
-    if (this.ticking.current) return;
+    if (this.ticking) return;
     window.requestAnimationFrame(() => {
       let fixedTop: number | false;
       const wrapToTop = this.affixWrapRef.current.getBoundingClientRect().top || 0;
       const wrapWidth = this.affixWrapRef.current.getBoundingClientRect().width || 0;
       const wrapHeight = this.affixWrapRef.current.getBoundingClientRect().height || 0;
       let containerToTop = 0;
-      if (this.scrollContainer.current instanceof HTMLElement) {
-        containerToTop = this.scrollContainer.current.getBoundingClientRect().top;
+      if (this.scrollContainer instanceof HTMLElement) {
+        containerToTop = this.scrollContainer.getBoundingClientRect().top;
       }
       const calcTop = wrapToTop - containerToTop;
       const containerHeight =
-        this.scrollContainer.current[this.scrollContainer.current instanceof Window ? 'innerHeight' : 'clientHeight'] -
-        wrapHeight;
-      const calcBottom = containerToTop + containerHeight - (this.inneroffsetBottom.value ?? 0);
-      if (this.innerOffsetTop.value !== undefined && calcTop <= this.innerOffsetTop.value) {
-        fixedTop = containerToTop + this.innerOffsetTop.value;
-      } else if (this.inneroffsetBottom.value !== undefined && wrapToTop >= calcBottom) {
+        this.scrollContainer[this.scrollContainer instanceof Window ? 'innerHeight' : 'clientHeight'] - wrapHeight;
+      const calcBottom = containerToTop + containerHeight - (this.inneroffsetBottom ?? 0);
+      if (this.innerOffsetTop !== undefined && calcTop <= this.innerOffsetTop) {
+        fixedTop = containerToTop + this.innerOffsetTop;
+      } else if (this.inneroffsetBottom !== undefined && wrapToTop >= calcBottom) {
         fixedTop = calcBottom;
       } else {
         fixedTop = false;
       }
       if (!this.affixRef) return;
       const affixed = fixedTop !== false;
-      let placeholderStatus = this.affixWrapRef.current.contains(this.placeholderEL.current);
+      let placeholderStatus = this.affixWrapRef.current.contains(this.placeholderEL);
       const prePlaceholderStatus = placeholderStatus;
       if (affixed) {
         this.affixRef.current.className = `${getClassPrefix()}-affix`;
         this.affixRef.current.style.top = `${fixedTop}px`;
         this.affixRef.current.style.width = `${wrapWidth}px`;
         this.affixRef.current.style.height = `${wrapHeight}px`;
-        if (this.innerZIndex.value) {
-          this.affixRef.current.style.zIndex = `${this.innerZIndex.value}`;
+        if (this.innerZIndex) {
+          this.affixRef.current.style.zIndex = `${this.innerZIndex}`;
         }
         if (!placeholderStatus) {
-          this.placeholderEL.current.style.width = `${wrapWidth}px`;
-          this.placeholderEL.current.style.height = `${wrapHeight}px`;
-          this.affixWrapRef.current.appendChild(this.placeholderEL.current);
+          this.placeholderEL.style.width = `${wrapWidth}px`;
+          this.placeholderEL.style.height = `${wrapHeight}px`;
+          this.affixWrapRef.current.appendChild(this.placeholderEL);
           placeholderStatus = true;
         }
       } else {
         this.affixRef.current.removeAttribute('class');
         this.affixRef.current.removeAttribute('style');
         if (placeholderStatus) {
-          this.placeholderEL.current.remove();
+          this.placeholderEL.remove();
           placeholderStatus = false;
         }
       }
       if (prePlaceholderStatus !== placeholderStatus && isFunction(this.props.onFixedChange)) {
         this.props.onFixedChange(affixed, { top: +fixedTop });
       }
-      this.ticking.current = false;
+      this.ticking = false;
     });
-    this.ticking.current = true;
+    this.ticking = true;
   };
 
   receiveProps(newProps: AffixProps) {
     const { offsetBottom, offsetTop, zIndex } = newProps;
-    this.innerOffsetTop.value = offsetTop;
-    this.inneroffsetBottom.value = offsetBottom;
-    this.innerZIndex.value = zIndex;
+    this.innerOffsetTop = offsetTop;
+    this.inneroffsetBottom = offsetBottom;
+    this.innerZIndex = zIndex;
     this.handleScroll();
     return true;
   }
 
   installed() {
     const { offsetBottom, offsetTop, zIndex, container } = this.props;
-    this.innerOffsetTop.value = offsetTop;
-    this.inneroffsetBottom.value = offsetBottom;
-    this.innerZIndex.value = zIndex;
-    this.placeholderEL.current = document.createElement('div');
-    this.scrollContainer.current = getScrollContainer(container);
-    if (this.scrollContainer.current) {
+    this.innerOffsetTop = offsetTop;
+    this.inneroffsetBottom = offsetBottom;
+    this.innerZIndex = zIndex;
+    this.placeholderEL = document.createElement('div');
+    this.scrollContainer = getScrollContainer(container);
+    if (this.scrollContainer) {
       this.handleScroll();
-      this.scrollContainer.current.addEventListener('scroll', this.handleScroll);
+      this.scrollContainer.addEventListener('scroll', this.handleScroll);
       window.addEventListener('resize', this.handleScroll);
     }
   }
 
   uninstalled() {
-    this.scrollContainer.current.removeEventListener('scroll', this.handleScroll);
+    this.scrollContainer.removeEventListener('scroll', this.handleScroll);
     window.removeEventListener('resize', this.handleScroll);
   }
 
