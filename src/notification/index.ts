92360aa783df7b0eeb5828dfdce9b7f71bd38b39
brightzhli <864345220@qq.com>
2024-09-04 11:52:14 +0800

    feat(notification): base example

---
+++ src/notification/index.ts
@@ -0,0 +1,8 @@
+import './style/index.js';
+
+import _Notification from './alert';
+
+export type { AlertProps } from './alert';
+
+export const Notification = _Notification;
+export default Notification;
