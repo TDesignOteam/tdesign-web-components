d3cef465bae48fcbe99f3ad387a3c2c97d194fe2
duenyang <377153400@qq.com>
2024-09-25 17:06:01 +0800

    fix(dropdown): dropdown问题修复

---
+++ src/select/defaultProps.ts
@@ -0,0 +1,28 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdOptionGroupProps, TdOptionProps, TdSelectProps } from './type';
+
+export const selectDefaultProps: TdSelectProps = {
+  autoWidth: false,
+  autofocus: false,
+  borderless: false,
+  clearable: false,
+  creatable: false,
+  loading: false,
+  max: 0,
+  minCollapsedNum: 0,
+  multiple: false,
+  placeholder: undefined,
+  readonly: false,
+  reserveKeyword: false,
+  showArrow: true,
+  size: 'medium',
+  status: 'default',
+  valueType: 'value',
+};
+
+export const optionDefaultProps: TdOptionProps = { checkAll: false, disabled: false };
+
+export const optionGroupDefaultProps: TdOptionGroupProps = { divider: true };
