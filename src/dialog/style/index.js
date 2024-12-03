335cc9d1a36c6bc5f44fe640caca63fc32a9dbb7
lincao <carolin913@126.com>
2024-12-03 04:00:44 -0600

    feat(slider,avatar,dialog): wc style split from common (#173)

--- src/dialog/style/index.js
+++ src/dialog/style/index.js
@@ -3,10 +3,12 @@ import { css, globalCSS } from 'omi';
 // 为了做主题切换
 import styles from '../../_common/style/web/components/dialog/_index.less';
 import dialogStyles from './dialog.less';
+import wcStyles from './wc.less';
 
 export const styleSheet = css`
   ${styles}
   ${dialogStyles}
+  ${wcStyles}
 `;
 
 globalCSS(styleSheet);
