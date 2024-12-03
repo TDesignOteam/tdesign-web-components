d89a00a1a5c5ba1bdc63eec173ff04727a15373a
carolin913 <carolin913@126.com>
2024-12-03 17:56:56 +0800

    feat(slider,avatar,dialog): wc style split from common

--- src/slider/style/index.js
+++ src/slider/style/index.js
@@ -2,9 +2,11 @@ import { css, globalCSS } from 'omi';
 
 // 为了做主题切换
 import styles from '../../_common/style/web/components/slider/_index.less';
+import wcStyles from './wc.less';
 
 export const styleSheet = css`
   ${styles}
+  ${wcStyles}
 `;
 
 globalCSS(styleSheet);
