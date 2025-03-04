146d6b14d369de761eb7a57936d242126ec1827c
brightzhli <864345220@qq.com>
2025-03-04 20:16:27 +0800

    feat(chatbot): 初步添加md渲染样式

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -1,8 +1,10 @@
 import './style/index.js';
 
+import _ChatItem from './ui/chat-item';
 import _Chat from './chat';
 
 export const Chat = _Chat;
+export const ChatItem = _ChatItem;
 export default Chat;
 
 export * from './type';
