54aeb9a74743cc1db11637609e26fe1e65c51bdb
carolin913 <carolin913@126.com>
2025-04-08 09:10:52 +0800

    feat(chatbot): export type

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -4,6 +4,7 @@ import reactifyWebComponent from 'omi-reactify';
 
 import _ChatItem from './ui/chat-item';
 import _Chat from './chat';
+import { TdChatProps } from './type';
 
 export const Chat = _Chat;
 export const ChatItem = _ChatItem;
@@ -11,7 +12,7 @@ export const ChatItem = _ChatItem;
 export * from './core';
 export * from './type';
 
-const ChatBot = reactifyWebComponent('t-chatbot');
+const ChatBot = reactifyWebComponent<TdChatProps>('t-chatbot');
 
 export { ChatBot };
 export default Chat;
