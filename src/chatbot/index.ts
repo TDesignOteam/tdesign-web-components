a921e99ca99920eabf5a0215bfb7bdf2c6b95d9d
duenyang <377153400@qq.com>
2025-04-17 22:07:45 +0800

    fix(chatbot): 修复chatbot类型问题

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -1,5 +1,6 @@
 import './style/index.js';
 
+import React from 'react';
 import reactifyWebComponent from 'omi-reactify';
 
 import _ChatItem from './ui/chat-item';
@@ -12,7 +13,8 @@ export const ChatItem = _ChatItem;
 export * from './core';
 export * from './type';
 
-const ChatBot = reactifyWebComponent<TdChatProps>('t-chatbot');
-
+const ChatBot: React.ForwardRefExoticComponent<
+  Omit<TdChatProps, 'ref'> & React.RefAttributes<HTMLElement | undefined>
+> = reactifyWebComponent<TdChatProps>('t-chatbot');
 export { ChatBot };
 export default Chat;
