14d03a309302c1715129e9c3b9b2efbb20cb2468
carolin913 <carolin913@126.com>
2025-04-21 20:50:34 +0800

    feat(chatbot): remove reactify

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -1,10 +1,9 @@
 import './style/index.js';
 
-import reactifyWebComponent from 'omi-reactify';
-
+// import reactifyWebComponent from 'omi-reactify';
 import _ChatItem from './ui/chat-item';
 import _Chat from './chat';
-import { TdChatProps } from './type';
+// import { TdChatProps } from './type';
 
 export const Chat = _Chat;
 export const ChatItem = _ChatItem;
@@ -12,7 +11,7 @@ export const ChatItem = _ChatItem;
 export * from './core';
 export * from './type';
 
-const ChatBot = reactifyWebComponent<TdChatProps>('t-chatbot');
+// const ChatBot = reactifyWebComponent<TdChatProps>('t-chatbot');
 
-export { ChatBot };
+// export { ChatBot };
 export default Chat;
