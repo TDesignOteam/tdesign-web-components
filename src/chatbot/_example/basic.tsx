1b2402d06fbd94eac1b40e9424fee8dd9f048991
lincao <carolin913@126.com>
2025-06-16 18:16:50 +0800

    feat(chatbot): onchatready (#237)

--- src/chatbot/_example/basic.tsx
+++ src/chatbot/_example/basic.tsx
@@ -508,6 +508,7 @@ export default class BasicChat extends Component {
             onFileSelect,
           }}
           chatServiceConfig={mockModels}
+          onChatReady={(e) => console.log('chatReady', e)}
         ></t-chatbot>
         <button onClick={() => this.chatRef.current?.setMessages(mockData, 'prepend')}>设置消息</button>
       </>
