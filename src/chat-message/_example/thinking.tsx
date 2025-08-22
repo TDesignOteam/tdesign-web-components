2d6233c72307ebcc67d43f77cb554080a6f6f4fb
brightzhli <864345220@qq.com>
2025-08-22 19:03:16 +0800

    feat(chatItem): thinking添加slot,修复markdown data为空时渲染object的问题

--- src/chat-message/_example/thinking.tsx
+++ src/chat-message/_example/thinking.tsx
@@ -18,17 +18,28 @@ export default class BasicExample extends Component {
 
   render() {
     return (
-      <t-chat-thinking-content
-        status="streaming"
-        content={{
-          title: '正在思考中...',
-          text: '好的，我现在需要回答用户关于近三年当代偶像爱情剧创作中需要规避的因素的问题。首先，我需要确定用户的问题类型，使用answer_framework_search查询对应的回答框架',
-        }}
-        maxHeight={50}
-        animation="moving"
-        collapsed={this.collapse.value}
-        onCollapsedChange={this.collapsedChangeHandle}
-      ></t-chat-thinking-content>
+      <div>
+        <t-chat-thinking-content
+          status="streaming"
+          content={{
+            title: '正在思考中...',
+            text: '好的，我现在需要回答用户关于近三年当代偶像爱情剧创作中需要规避的因素的问题。首先，我需要确定用户的问题类型，使用answer_framework_search查询对应的回答框架',
+          }}
+          maxHeight={50}
+          animation="moving"
+          collapsed={this.collapse.value}
+          onCollapsedChange={this.collapsedChangeHandle}
+        />
+        <t-chat-thinking-content
+          content={{
+            title: '正在思考中...',
+          }}
+          status="streaming"
+          animation="moving"
+        >
+          <div slot="content">自定义content</div>
+        </t-chat-thinking-content>
+      </div>
     );
   }
 }
