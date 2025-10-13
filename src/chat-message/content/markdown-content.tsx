fc78a4a73dd48ea9ba051500965c4fa50023844e
阿菜 Cai <jimmyrss1102@gmail.com>
2025-10-13 19:55:25 +0800

    fix:  cherry-markdown mermaid render error

--- src/chat-message/content/markdown-content.tsx
+++ src/chat-message/content/markdown-content.tsx
@@ -1,8 +1,10 @@
 import '../md/chat-md-code';
 
+import CherryCodeBlockMermaidPlugin from 'cherry-markdown/dist/addons/cherry-code-block-mermaid-plugin';
 import Cherry from 'cherry-markdown/dist/cherry-markdown.core';
 import type { CherryOptions } from 'cherry-markdown/types/cherry';
 import { escape, merge } from 'lodash-es';
+import mermaid from 'mermaid';
 import { Component, createRef, signal, tag } from 'omi';
 
 import { getClassPrefix } from '../../_util/classname';
@@ -12,6 +14,11 @@ import { AddPartHook } from '../md/utils';
 import styles from '../style/chat-content.less';
 // 单独用该组件时，发现动态加载样式不生效，目前直接引入
 import codeStyles from '../style/md/chat-md-code.less';
+
+Cherry?.usePlugin?.(CherryCodeBlockMermaidPlugin, {
+  mermaid,
+  mermaidCanvasAppendDom: document.querySelector('#app'),
+});
 
 const baseClass = `${getClassPrefix()}-chat__text`;
 
@@ -122,12 +129,12 @@ export default class ChatCherryMDContent extends Component<TdChatMarkdownContent
           codeBlock: {
             customRenderer: {
               // 自定义语法渲染器
-              all: {
-                render: (code, _sign, _cherry, lang) =>
-                  `<t-chat-md-code key="${_sign}" data-lang="${lang}" data-code="${escape(code)}" data-theme="${
-                    this.markdownOptions.themeSettings?.codeBlockTheme === 'dark' ? 'dark' : 'light'
-                  }" />`,
-              },
+              // all: {
+              //   render: (code, _sign, _cherry, lang) =>
+              //     `<t-chat-md-code key="${_sign}" data-lang="${lang}" data-code="${escape(code)}" data-theme="${
+              //       this.markdownOptions.themeSettings?.codeBlockTheme === 'dark' ? 'dark' : 'light'
+              //     }" />`,
+              // },
             },
           },
         },
@@ -141,14 +148,13 @@ export default class ChatCherryMDContent extends Component<TdChatMarkdownContent
 
   getTextInfo() {
     const { content } = this.props;
-    if (typeof content !== 'string') return;
-    // 这里给一个空格针对空内容占位，避免cherryMD复用旧数据
-    this.parseMarkdown(content || ' ');
+    if (typeof content !== 'string') return '';
+    return this.parseMarkdown(content);
   }
 
   parseMarkdown(markdown: string) {
     if (!this.isMarkdownInit.value || !markdown) return '';
-    this.md?.setMarkdown(markdown);
+    return this.md?.setMarkdown(markdown);
   }
 
   render() {
