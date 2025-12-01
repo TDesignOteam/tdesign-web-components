8c7d6ae7246fc0111471ad22adba45f49bef29cf
LeonardoSya <2246866774@qq.com>
2025-12-01 17:05:10 +0800

    feat: use signal

--- src/select/_example/loading.tsx
+++ src/select/_example/loading.tsx
@@ -1,30 +1,29 @@
 import 'tdesign-web-components/select';
 import 'tdesign-web-components/space';
 
-import { Component } from 'omi';
+import { Component, signal } from 'omi';
 
 export default class LoadingDemo extends Component {
-  options = [];
+  options = signal([]);
 
-  loading = false;
+  loading = signal(false);
 
-  value: string | undefined = undefined;
+  value = signal(undefined);
 
   handlePopupVisibleChange = (visible: boolean) => {
     if (visible) {
-      this.loading = true;
-      this.options = [];
-      this.value = '';
-      this.update();
+      this.loading.value = true;
+      this.options.value = [];
+      this.value.value = '';
 
+      // 模拟异步请求延迟
       setTimeout(() => {
-        this.loading = false;
-        this.options = [
+        this.loading.value = false;
+        this.options.value = [
           { label: '选项一', value: '1' },
           { label: '选项二', value: '2' },
           { label: '选项三', value: '3' },
         ];
-        this.update();
       }, 1000);
     }
   };
@@ -34,14 +33,13 @@ export default class LoadingDemo extends Component {
       <t-space>
         <t-select
           placeholder="请选择"
-          loading={this.loading}
-          options={this.options}
-          value={this.value}
+          loading={this.loading.value}
+          options={this.options.value}
+          value={this.value.value}
           onPopupVisibleChange={this.handlePopupVisibleChange}
           onChange={(value, context) => {
-            this.value = value;
+            this.value.value = value;
             console.log('change', value, context);
-            this.update();
           }}
         />
       </t-space>
