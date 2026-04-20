cde57be9ab3fbb41d425e160df19f9418219e15e
brightzhli <864345220@qq.com>
2026-04-20 12:42:29 +0800

    feat(shared): migrate _util utilities from feat/chat

--- packages/shared/src/_util/classname.ts
+++ packages/shared/src/_util/classname.ts
@@ -1,8 +1,8 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+// import { twMerge } from 'tailwind-merge';
 
 export default function classname(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(inputs);
 }
 
 export function getClassPrefix() {
