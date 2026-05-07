99828b8f3dd285079500f0fa55eac9bf8e6182e3
阿菜 Cai <jimmyrss1102@gmail.com>
2026-05-07 10:53:05 +0800

    refactor(shared): 引用公共库中的easing模块

--- packages/shared/src/_util/easing.ts
+++
@@ -1,3 +0,0 @@
-// Re-export from @common to avoid duplication with submodule
-export { linear, easeInOutCubic } from '@common/js/utils/easing';
-export type { EasingFunction } from '@common/js/utils/easing';
