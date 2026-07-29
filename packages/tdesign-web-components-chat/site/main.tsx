f7868dd05a842a44173f1cb27cd8c0fe39aa2ea9
黎伟杰 <674416404@qq.com>
2026-07-29 09:40:09 +0800

    build(deps): 更换 theme-generator 包名及版本

--- packages/tdesign-web-components-chat/site/main.tsx
+++ packages/tdesign-web-components-chat/site/main.tsx
@@ -1,5 +1,5 @@
 import '@tdesign/site-components';
-import 'tdesign-theme-generator';
+import '@tdesign/theme-generator';
 import '@tdesign/site-components/lib/styles/style.css';
 import './tailwind/index.ts';
 import '@tdesign/web-components/style/index.js';
