9e4abbcafc2e7d49541ea57b040eebb1ad0e13a7
liweijie <674416404@qq.com>
2026-08-25 01:22:21 +0800

    chore(chat-loading): 简化示例代码结构

--- packages/pro-components/chat/chat-loading/_example/chat-loading-text.tsx
+++ packages/pro-components/chat/chat-loading/_example/chat-loading-text.tsx
@@ -6,11 +6,9 @@ import { Component } from 'omi';
 export default class ChatLoading extends Component {
   render() {
     return (
-      <>
-        <t-space size={60}>
-          <t-chat-loading animation="moving" text={'思考中...'}></t-chat-loading>
-        </t-space>
-      </>
+      <t-space>
+        <t-chat-loading animation="moving" text={'思考中...'}></t-chat-loading>
+      </t-space>
     );
   }
 }
