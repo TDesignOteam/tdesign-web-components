f401185c76002a2531881c58eaa65051013cf066
brightzhli <864345220@qq.com>
2026-08-25 16:16:09 +0800

    feat(chat-item): append slot wrapper 补充部分content的slot包裹，支持相同type自定义     migrate from https://github.com/TDesignOteam/tdesign-web-components/pull/342

--- packages/pro-components/chat/chat-message/chat-item.tsx
+++ packages/pro-components/chat/chat-message/chat-item.tsx
@@ -233,9 +233,9 @@ export default class ChatItem extends Component<ChatMessageProps> {
       // 系统消息渲染
       if (role === 'system') {
         return (
-          <div key={elementKey} className={`${getClassPrefix()}-chat__text--${role}`}>
-            {content.data}
-          </div>
+          <slot key={elementKey} name={`${content.type}-${index}`}>
+            <div className={`${getClassPrefix()}-chat__text--${role}`}>{content.data}</div>
+          </slot>
         );
       }
       // 用户消息渲染
@@ -249,9 +249,9 @@ export default class ChatItem extends Component<ChatMessageProps> {
         }
         if (isTextContent(content) || isMarkdownContent(content)) {
           return (
-            <div key={elementKey} className={`${getClassPrefix()}-chat__text--${role}`}>
-              {content.data}
-            </div>
+            <slot key={elementKey} name={`${content.type}-${index}`}>
+              <div className={`${getClassPrefix()}-chat__text--${role}`}>{content.data}</div>
+            </slot>
           );
         }
         return <slot key={elementKey} name={`${content?.slotName || `${content.type}-${index}`}`}></slot>;
@@ -317,20 +317,24 @@ export default class ChatItem extends Component<ChatMessageProps> {
           // 图片
           const { url, name } = content.data;
           return (
-            <div key={elementKey} className={`${className}__image`}>
-              <img src={url} alt={name} width={200} height={200}></img>
-            </div>
+            <slot key={elementKey} name={`${content.type}-${index}`}>
+              <div key={elementKey} className={`${className}__image`}>
+                <img src={url} alt={name} width={200} height={200}></img>
+              </div>
+            </slot>
           );
         }
         if (isTextContent(content) || isMarkdownContent(content)) {
           // 正文回答
           return (
-            <t-chat-md-content
-              key={elementKey}
-              className={`${className}__detail`}
-              {...chatContentProps?.markdown}
-              content={content.data}
-            ></t-chat-md-content>
+            <slot key={elementKey} name={`${content.type}-${index}`}>
+              <t-chat-md-content
+                key={elementKey}
+                className={`${className}__detail`}
+                {...chatContentProps?.markdown}
+                content={content.data}
+              ></t-chat-md-content>
+            </slot>
           );
         }
         // 自定义渲染slot
